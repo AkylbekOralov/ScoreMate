df6d036ce4cae93d7b72a73c10e2f087b7d7f3e1
Akylbek <akylbek.oralov@nu.edu.kz>
2025-01-23 12:42:05 +0500

    Initial Commit

---
+++ ScoreMate/ScoreMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScoreMateApp.swift
+//  ScoreMate
+//
+//  Created by Akylbek Oralov on 23.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct ScoreMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
