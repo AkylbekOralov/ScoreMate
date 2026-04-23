ca3e9dc41e631c9a97c78667ce63a8cc6822415a
Akylbek Oralov <oralovv26@gmail.com>
2026-04-24 00:35:16 +0500

    Setted up swift debug tool

--- ScoreMate/ScoreMateApp.swift
+++ ScoreMate/ScoreMateApp.swift
@@ -6,12 +6,43 @@
 //
 
 import SwiftUI
+import UIKit
+
+#if canImport(DebugSwift)
+import DebugSwift
+#endif
+
+final class AppDelegate: NSObject, UIApplicationDelegate {
+    #if DEBUG
+    let debugSwift = DebugSwift()
+    #endif
+
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
+    ) -> Bool {
+        #if DEBUG
+        debugSwift.setup()
+        debugSwift.show()
+        #endif
+
+        return true
+    }
+
+    #if DEBUG
+    func toggleDebugSwift() {
+        debugSwift.toggle()
+    }
+    #endif
+}
 
 @main
 struct ScoreMateApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) private var appDelegate
+
     var body: some Scene {
         WindowGroup {
-           MainView()
+            MainView()
         }
     }
 }
--- ScoreMate/Screens/MainScreen/MainView.swift
+++ ScoreMate/Screens/MainScreen/MainView.swift
@@ -6,6 +6,41 @@
 //
 
 import SwiftUI
+import UIKit
+
+private struct ShakeDetector: UIViewControllerRepresentable {
+    let onShake: () -> Void
+
+    func makeUIViewController(context: Context) -> ShakeDetectorViewController {
+        let controller = ShakeDetectorViewController()
+        controller.onShake = onShake
+        return controller
+    }
+
+    func updateUIViewController(_ uiViewController: ShakeDetectorViewController, context: Context) {
+        uiViewController.onShake = onShake
+    }
+}
+
+private final class ShakeDetectorViewController: UIViewController {
+    var onShake: (() -> Void)?
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        becomeFirstResponder()
+    }
+
+    override var canBecomeFirstResponder: Bool {
+        true
+    }
+
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        super.motionEnded(motion, with: event)
+
+        guard motion == .motionShake else { return }
+        onShake?()
+    }
+}
 
 struct MainView: View {
     @State var leaguesViewModel = LeaguesViewModel()
@@ -31,5 +66,13 @@ struct MainView: View {
         }
         .accentColor(.primary)
         .preferredColorScheme(selectedTheme.colorScheme)
+        .background {
+            #if DEBUG
+            ShakeDetector {
+                (UIApplication.shared.delegate as? AppDelegate)?.toggleDebugSwift()
+            }
+            .allowsHitTesting(false)
+            #endif
+        }
     }
 }
